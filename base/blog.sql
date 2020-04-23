673e4fb4708238960396b0b4acc4d4f259b06045
Rolls98 <toureb16@gmail.com>
2020-04-23 17:27:24 +0000

    mise a jour de la bd

--- base/blog.sql
+++ base/blog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2deb1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:3306
--- Généré le :  lun. 20 avr. 2020 à 14:16
--- Version du serveur :  10.3.22-MariaDB-1
--- Version de PHP :  7.3.12-1
+-- Hôte : 127.0.0.1
+-- Généré le :  jeu. 23 avr. 2020 à 19:26
+-- Version du serveur :  10.1.37-MariaDB
+-- Version de PHP :  7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,35 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `blog`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL,
+  `addresse` varchar(255) NOT NULL DEFAULT '',
+  `ville` varchar(255) NOT NULL DEFAULT '',
+  `pays` varchar(255) NOT NULL DEFAULT '',
+  `description` text NOT NULL,
+  `image` varchar(255) NOT NULL DEFAULT 'default_icon.jpg',
+  `a_password` varchar(255) NOT NULL,
+  `update_psd` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `nom`, `prenom`, `email`, `addresse`, `ville`, `pays`, `description`, `image`, `a_password`, `update_psd`, `created_date`) VALUES
+(1, 'anleboss2.19', 'Aniki', 'leBoss', 'Aniki@leboss.com', 'BP KS 105', 'Abidjan', 'Côte D\'ivoire', 'Description\r\nLorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus unde quos velit optio eius rerum quia perspiciatis et voluptatum, voluptates quod impedit neque quisquam delectus similique repellat in blanditiis officia!.', 'default_icon.jpg', 'f600fdf5deebf577267fdaa9e54df05a417c6da0444e124f0aa84fa5aaf721ba', '2020-04-22 23:45:18', '2020-04-22 23:45:18');
 
 -- --------------------------------------------------------
 
@@ -44,7 +73,9 @@ CREATE TABLE `article` (
 INSERT INTO `article` (`id`, `titre`, `s_titre`, `description`, `image`, `date_create`) VALUES
 (1, 'la fois ou j\'ai', 'failli y rester', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum similique quo omnis delectus sequi voluptate minima ullam, aspernatur aut! Ut libero similique inventore doloribus tenetur cum alias, corrupti facere autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nam, esse enim tempora ratione quo in vero corrupti nobis fugit voluptatum temporibus optio quidem? Illum distinctio consequuntur expedita repellat dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt distinctio inventore nostrum? Perspiciatis quisquam est magnam autem porro laborum, tempora, perferendis maiores distinctio totam eaque similique, amet omnis? Assumenda, saepe?.', 'products-02-01.jpg', '2020-04-16 15:05:36'),
 (2, 'Les risques d\'overdeuses', 'LE BLACKOUT', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum similique quo omnis delectus sequi voluptate minima ullam, aspernatur aut! Ut libero similique inventore doloribus tenetur cum alias, corrupti facere autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nam, esse enim tempora ratione quo in vero corrupti nobis fugit voluptatum temporibus optio quidem? Illum distinctio consequuntur expedita repellat dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt distinctio inventore nostrum? Perspiciatis quisquam est magnam autem porro laborum, tempora, perferendis maiores distinctio totam eaque similique, amet omnis? Assumenda, saepe?.', 'products-03.jpg', '2020-04-16 23:30:34'),
-(3, 'Un test', 'Ajout Article', 'lorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem lorem', '0.png', '2020-04-17 12:42:59');
+(3, 'Un test', 'Ajout Article', 'lorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem lorem', '0.png', '2020-04-17 12:42:59'),
+(4, 'Le bon', 'Garba', 'la meilleure nourriture des ivoirien,\r\nchampion, tu es un abidjanais, tu n\'as jamais mang&amp;eacute; garba, c\'est que t\'es mazo', 'c502176e6e0309d9c191bd246f112d15.jpg', '2020-04-20 18:02:49'),
+(5, 'La Drogue', 'Un comprim&amp;eacute; qui tue', 'la drogue a tu&amp;eacute; plus de 25600 personnes en Europe', 'rBVaSltxOo2ADKsIAAFfAu6_gMU586.jpg', '2020-04-23 15:49:28');
 
 -- --------------------------------------------------------
 
@@ -59,10 +90,10 @@ CREATE TABLE `clients` (
   `prenom` varchar(255) NOT NULL DEFAULT '',
   `email` varchar(255) NOT NULL,
   `age` int(11) NOT NULL,
-  `addresse` text NOT NULL DEFAULT '',
+  `addresse` text NOT NULL,
   `ville` varchar(255) NOT NULL DEFAULT '',
   `pays` varchar(255) NOT NULL DEFAULT '',
-  `description` text NOT NULL DEFAULT '',
+  `description` text NOT NULL,
   `u_password` varchar(255) NOT NULL,
   `image` text NOT NULL,
   `date_inscription` datetime NOT NULL
@@ -73,8 +104,9 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`id`, `username`, `nom`, `prenom`, `email`, `age`, `addresse`, `ville`, `pays`, `description`, `u_password`, `image`, `date_inscription`) VALUES
-(1, 'Rolls98', 'Ouattara', 'Mory', 'Rolls@yopmail.com', 20, 'BP 10 Abj 225', 'Abidjan', 'Côte D\'ivoire', 'je suis timide,curieux,humble.\r\n\r\nj\'aime beaucoup la programmation, j\'adore aussi le travail, et je travail avec serieux', 'e24df920078c3dd4e7e8d2442f00e5c9ab2a231bb3918d65cc50906e49ecaef4', '3 20-04-2020 13-58-05.jpg', '2020-04-16 20:13:29'),
-(2, '', 'aniki', '', 'anikileboss@bigboss.com', 28, '', '', '', '', '36f50957f5e0b6ee3ef455674da35a86667f3314209dc1514c510fe95e840831', 'default_icon.jpg', '2020-04-19 01:54:53');
+(1, 'Rolls98', 'Mory', 'Ouattara', 'Rolls@yopmail.com', 20, 'Abj 435 bp ks', 'Abidjan', 'Côte D\'ivoire', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus unde quos velit optio eius rerum quia perspiciatis et voluptatum, voluptates quod impedit neque quisquam delectus similique repellat in blanditiis officia!.', 'e24df920078c3dd4e7e8d2442f00e5c9ab2a231bb3918d65cc50906e49ecaef4', 'IMG-20200416-WA0057.jpg', '2020-04-16 20:13:29'),
+(2, '', 'aniki', '', 'anikileboss@bigboss.com', 28, '', '', '', '', '36f50957f5e0b6ee3ef455674da35a86667f3314209dc1514c510fe95e840831', 'default_icon.jpg', '2020-04-19 01:54:53'),
+(3, '', 'test', '', 'test@test.com', 23, '', '', '', '', '60e27eb47f89048b3412b4f94149522a9fea236140e1245e8a7bd211e1e108d8', 'default_icon.jpg', '2020-04-20 17:59:57');
 
 -- --------------------------------------------------------
 
@@ -105,13 +137,19 @@ CREATE TABLE `service_client` (
   `pays` varchar(255) NOT NULL,
   `ville` varchar(255) NOT NULL,
   `message` text NOT NULL,
-  `abonne` tinyint(1) NOT NULL DEFAULT 0,
+  `abonne` tinyint(1) NOT NULL DEFAULT '0',
   `date_envoi` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `article`
@@ -143,16 +181,22 @@ ALTER TABLE `service_client`
 --
 
 --
+-- AUTO_INCREMENT pour la table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT pour la table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `forum`
@@ -164,7 +208,7 @@ ALTER TABLE `forum`
 -- AUTO_INCREMENT pour la table `service_client`
 --
 ALTER TABLE `service_client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
